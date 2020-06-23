8b061d98ab3b15e42a9f5786484d7d217a345cc0
Your Name <you@example.com>
2020-06-23 14:26:28 -0300

    Escrevendo views

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
 
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Olá Mundo. Você está no índice de pesquisas.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "Você está vendo os resultados da pergunta %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Você está votando na pergunta %s." % question_id)
+
